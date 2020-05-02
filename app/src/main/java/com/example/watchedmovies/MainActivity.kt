8218d7d7da5e644853e597ae9e046666c1534a17
Alex Simonov <sim_aa@mail.ru>
2020-05-03 00:36:27 +0300

    Выровнял кнопки

--- app/src/main/java/com/example/watchedmovies/MainActivity.kt
+++ app/src/main/java/com/example/watchedmovies/MainActivity.kt
@@ -3,11 +3,22 @@ package com.example.watchedmovies
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Поиск вьюх
+        val testMainTextView: TextView = testMainTextView
+        val watchedMovieButton: Button = watched_movies_button
+        val addMovieButton: Button = add_movie_button
+
+        addMovieButton.setOnClickListener { testMainTextView.text = "Добавить" }
+        watchedMovieButton.setOnClickListener { testMainTextView.text = "Просмотренные" }
     }
 }
