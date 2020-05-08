bb8d791b40f6e03b1262afd4859fc64c4f7e39a1
Alex Simonov <sim_aa@mail.ru>
2020-05-08 23:55:36 +0300

    добавил Fab

---
+++ app/src/main/java/com/example/watchedmovies/MoviesFeed.kt
@@ -0,0 +1,19 @@
+package com.example.watchedmovies
+
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_movies_feed.*
+
+class MoviesFeed : AppCompatActivity(R.layout.activity_movies_feed) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val toast = Toast.makeText(applicationContext, "FAB", Toast.LENGTH_SHORT)
+        fab.setOnClickListener { toast.show() }
+    }
+
+}
