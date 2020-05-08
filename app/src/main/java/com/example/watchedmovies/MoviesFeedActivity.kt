ef603a35291f3d8598ed2feef23af592570542e1
Alex Simonov <sim_aa@mail.ru>
2020-05-09 01:12:42 +0300

    добавил startActivityForResult

--- app/src/main/java/com/example/watchedmovies/MoviesFeedActivity.kt
+++ app/src/main/java/com/example/watchedmovies/MoviesFeedActivity.kt
@@ -1,19 +1,25 @@
 package com.example.watchedmovies
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_movies_feed.*
 
+const val REQUEST_CODE_MOVIE_EDIT: Int = 1;
+
 class MoviesFeedActivity : AppCompatActivity(R.layout.activity_movies_feed) {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val toast = Toast.makeText(applicationContext, "FAB", Toast.LENGTH_SHORT)
-        fab.setOnClickListener { toast.show() }
+        fab.setOnClickListener {
+            val intent = Intent(this, MovieinfoEditorActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE_MOVIE_EDIT)
+        }
     }
 
 }
