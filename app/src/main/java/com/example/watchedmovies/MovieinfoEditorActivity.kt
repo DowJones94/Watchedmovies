4215b516afa3714a2e188a9166c38f36a568486e
Alex Simonov <sim_aa@mail.ru>
2020-05-29 23:47:12 +0300

    добавил setResult, поправил валидацию формы

--- app/src/main/java/com/example/watchedmovies/MovieinfoEditorActivity.kt
+++ app/src/main/java/com/example/watchedmovies/MovieinfoEditorActivity.kt
@@ -1,8 +1,12 @@
 package com.example.watchedmovies
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import kotlinx.android.synthetic.main.activity_editor.*
+
 
 class MovieinfoEditorActivity : AppCompatActivity(R.layout.activity_editor) {
 
@@ -15,22 +19,42 @@ class MovieinfoEditorActivity : AppCompatActivity(R.layout.activity_editor) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        btnAddMovie.setOnClickListener { checkEditText() }
+        btnAddMovie.setOnClickListener {
+            if (validateFields()) {
+                val intent = Intent()
+                intent.putExtra("MOVIE_TITLE", etMovieTitle.text.trim().toString())
+                intent.putExtra("MOVIE_LIKE", etLike.text.trim().toString())
+                intent.putExtra("MOVIE_DISLIKE", etDislike.text.trim().toString())
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            }
+        }
+
+        /* Сброс ошибок */
+        etMovieTitle.doAfterTextChanged { etMovieTitle.error = null }
+        etLike.doAfterTextChanged { etLike.error = null }
+        etDislike.doAfterTextChanged { etDislike.error = null }
     }
 
-    private fun checkEditText(){
+    private fun validateFields() : Boolean {
+
+        var bIsFilled = true
 
         if (etMovieTitle.text.trim().isEmpty()) {
             etMovieTitle.error = "Обязательное поле"
+            bIsFilled = false
         }
 
         if (etLike.text.trim().isEmpty()) {
             etLike.error = "Обязательное поле"
+            bIsFilled = false
         }
 
         if (etDislike.text.trim().isEmpty()) {
             etDislike.error = "Обязательное поле"
+            bIsFilled = false
         }
 
+        return bIsFilled
     }
 }
