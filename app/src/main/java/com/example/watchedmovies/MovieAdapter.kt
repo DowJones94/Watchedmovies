733468075965b554d31ecb27af64a23baf9868ff
Alexander Simonov <sim_aa_code@mail.ru>
2020-06-19 17:32:12 +0300

    Переписал адаптер, добавил листнер

--- app/src/main/java/com/example/watchedmovies/MovieAdapter.kt
+++ app/src/main/java/com/example/watchedmovies/MovieAdapter.kt
@@ -1,39 +1,38 @@
 package com.example.watchedmovies
 
-import android.content.Context
-import android.provider.ContactsContract.CommonDataKinds.Phone
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.movie_item.view.*
 
 
-class MovieAdapter(context : Context, movies : List<MovieinfoEditorActivity.Movie>) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
-
-    private val inflater = LayoutInflater.from(context)
-    private val movies = movies
+class MovieAdapter(val movies : List<MovieinfoEditorActivity.Movie>, val callback : Callback) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView: View = inflater.inflate(R.layout.movie_item, parent, false)
+        val itemView: View = LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
         return ViewHolder(itemView)
     }
 
-    override fun getItemCount(): Int {
-        return movies.size
-    }
+    override fun getItemCount() = movies.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val movie = movies.get(position)
-        holder.tvTitle.text = movie.title
-        holder.tvDislike.text = movie.negativeComment
-        holder.tvLike.text = movie.positiveComment
+        holder.bind()
     }
 
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+            itemView.setOnClickListener { callback.onMovieItemClicked(adapterPosition) }
+        }
+        fun bind() {
+            val movie = movies[adapterPosition]
+            itemView.tvTitle.text = movie.title
+            itemView.tvDislike.text = movie.negativeComment
+            itemView.tvLike.text = movie.positiveComment
+        }
+    }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvTitle : TextView = itemView.findViewById(R.id.tvTitle)
-        val tvDislike : TextView = itemView.findViewById(R.id.tvDislike)
-        val tvLike : TextView = itemView.findViewById(R.id.tvLike)
+    interface Callback {
+        fun onMovieItemClicked(position: Int)
     }
 }
--- app/src/main/java/com/example/watchedmovies/MoviesFeedActivity.kt
+++ app/src/main/java/com/example/watchedmovies/MoviesFeedActivity.kt
@@ -2,6 +2,7 @@ package com.example.watchedmovies
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
@@ -10,7 +11,7 @@ import kotlinx.android.synthetic.main.activity_movies_feed.*
 
 const val REQUEST_CODE_MOVIE_EDIT: Int = 1
 
-class MoviesFeedActivity : AppCompatActivity(R.layout.activity_movies_feed) {
+class MoviesFeedActivity : AppCompatActivity(R.layout.activity_movies_feed), MovieAdapter.Callback {
 
     //Лист из фильмов (data class Movie из MovieinfoEditorActivity)
     private val movies : ArrayList<MovieinfoEditorActivity.Movie> = arrayListOf()
@@ -21,8 +22,7 @@ class MoviesFeedActivity : AppCompatActivity(R.layout.activity_movies_feed) {
         setMoviesData() //Задаем начальные данные для элементов списка
 
         //Привязываем адаптер к RecyclerView и передаем данные в адаптер
-        val recyclerView = findViewById<View>(R.id.movie_recycler_view) as RecyclerView
-        recyclerView.adapter = MovieAdapter(this, movies)
+        movie_recycler_view.adapter = MovieAdapter(movies, this)
 
         //Кнопка с плюсом
         fab.setOnClickListener {
@@ -35,5 +35,9 @@ class MoviesFeedActivity : AppCompatActivity(R.layout.activity_movies_feed) {
         for(i in 1..100){
             movies.add(MovieinfoEditorActivity.Movie("Заголовок $i", "Не понравилось $i", "Понравилось $i"))
         }
+    }
+
+    override fun onMovieItemClicked(position: Int) {
+        Log.i("ITEM_MOVIE", "onMovieItemClicked: $position")
     }
 }
