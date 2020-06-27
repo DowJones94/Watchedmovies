10820ce555e3f852cca1e53927f9641e6316bf99
Alex Simonov <sim_aa@mail.ru>
2020-06-27 18:03:36 +0300

    Прописал анимацию кнопки

--- app/src/main/java/com/example/watchedmovies/MovieinfoEditorActivity.kt
+++ app/src/main/java/com/example/watchedmovies/MovieinfoEditorActivity.kt
@@ -4,6 +4,7 @@ import android.animation.ValueAnimator
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.doOnLayout
@@ -14,17 +15,20 @@ import kotlinx.android.synthetic.main.activity_editor.*
 class MovieinfoEditorActivity : AppCompatActivity(R.layout.activity_editor) {
 
     private val animator = ValueAnimator.ofFloat(0.0F, 1.0F).also {
-        it.duration = 1000
+        it.duration = 900
         it.interpolator = AccelerateDecelerateInterpolator()
         it.addUpdateListener {
             val animVal = it.animatedValue as Float
             val targetTranslationX = 0
-            etMovieTitle.alpha = 0.2F + 0.8F*animVal
+            val targetTranslationY = 0
+            etMovieTitle.alpha = animVal
             etMovieTitle.translationX += (targetTranslationX - etMovieTitle.translationX) * animVal
-            etLike.alpha = 0.2F + 0.8F*animVal
+            etLike.alpha = animVal
             etLike.translationX += (targetTranslationX - etLike.translationX) * animVal
-            etDislike.alpha = 0.2F + 0.8F*animVal
+            etDislike.alpha = animVal
             etDislike.translationX += (targetTranslationX - etDislike.translationX) * animVal
+            btnAddMovie.alpha = animVal
+            btnAddMovie.translationY += (targetTranslationY - btnAddMovie.translationY) * animVal
         }
     }
 
@@ -75,17 +79,21 @@ class MovieinfoEditorActivity : AppCompatActivity(R.layout.activity_editor) {
     }
 
     private fun setViewSettingsForAnimation(){
-        etMovieTitle.alpha = 0.2F
+        etMovieTitle.alpha = 0F
         etMovieTitle.doOnLayout {
-            it.translationX = -it.width.toFloat()
+            it.translationX = it.width.toFloat()
         }
-        etLike.alpha = 0.2F
+        etLike.alpha = 0F
         etLike.doOnLayout {
-            it.translationX = -it.width.toFloat()
+            it.translationX = it.width.toFloat()
         }
-        etDislike.alpha = 0.2F
+        etDislike.alpha = 0F
         etDislike.doOnLayout {
-            it.translationX = -it.width.toFloat()
+            it.translationX = it.width.toFloat()
+        }
+        btnAddMovie.alpha = 0F
+        btnAddMovie.doOnLayout {
+            it.translationY = (it.parent as View).height.toFloat()
         }
     }
 }
