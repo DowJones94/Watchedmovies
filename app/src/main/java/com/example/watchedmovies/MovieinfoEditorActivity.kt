eb07cc227b61b6bbf673722d82623a6d5e7997b4
Alex Simonov <sim_aa@mail.ru>
2020-06-27 14:04:19 +0300

    Прописал анимацию текстовых полей

--- app/src/main/java/com/example/watchedmovies/MovieinfoEditorActivity.kt
+++ app/src/main/java/com/example/watchedmovies/MovieinfoEditorActivity.kt
@@ -4,19 +4,34 @@ import android.animation.ValueAnimator
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.doOnLayout
 import androidx.core.widget.doAfterTextChanged
 import kotlinx.android.synthetic.main.activity_editor.*
 
 
 class MovieinfoEditorActivity : AppCompatActivity(R.layout.activity_editor) {
 
-    private lateinit var animator : ValueAnimator
+    private val animator = ValueAnimator.ofFloat(0.0F, 1.0F).also {
+        it.duration = 1000
+        it.interpolator = AccelerateDecelerateInterpolator()
+        it.addUpdateListener {
+            val animVal = it.animatedValue as Float
+            val targetTranslationX = 0
+            etMovieTitle.alpha = 0.2F + 0.8F*animVal
+            etMovieTitle.translationX += (targetTranslationX - etMovieTitle.translationX) * animVal
+            etLike.alpha = 0.2F + 0.8F*animVal
+            etLike.translationX += (targetTranslationX - etLike.translationX) * animVal
+            etDislike.alpha = 0.2F + 0.8F*animVal
+            etDislike.translationX += (targetTranslationX - etDislike.translationX) * animVal
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        animator = ValueAnimator.ofFloat(0.0F, 1.0F).also{ it.addUpdateListener {  } }
+        setViewSettingsForAnimation()
         animator.start()
 
         btnAddMovie.setOnClickListener {
@@ -57,5 +72,20 @@ class MovieinfoEditorActivity : AppCompatActivity(R.layout.activity_editor) {
         }
 
         return bIsFilled
+    }
+
+    private fun setViewSettingsForAnimation(){
+        etMovieTitle.alpha = 0.2F
+        etMovieTitle.doOnLayout {
+            it.translationX = -it.width.toFloat()
+        }
+        etLike.alpha = 0.2F
+        etLike.doOnLayout {
+            it.translationX = -it.width.toFloat()
+        }
+        etDislike.alpha = 0.2F
+        etDislike.doOnLayout {
+            it.translationX = -it.width.toFloat()
+        }
     }
 }
