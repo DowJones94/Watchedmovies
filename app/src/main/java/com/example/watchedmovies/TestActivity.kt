873e71d5cb50c3311a124553f0302441ea6d572a
Alexander Simonov <sim_aa_code@mail.ru>
2020-07-03 16:24:51 +0300

    сделал возможным перетаскивание TextView

--- app/src/main/java/com/example/watchedmovies/TestActivity.kt
+++ app/src/main/java/com/example/watchedmovies/TestActivity.kt
@@ -1,22 +1,38 @@
 package com.example.watchedmovies
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_test.*
 
 
 class TestActivity : AppCompatActivity() {
+    private var x0 = 0F
+    private var y0 = 0F
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
 
-        testFrameLayout.setOnTouchListener OnTouchListener@{ _, event ->
-            val x = String.format("%.2f", event.x)
-            val y = String.format("%.2f", event.y)
-
-            myTextView2.text = "X: $x Y: $y"
+        //Выводим координаты касаний по FrameLayout и событие action
+        /*testFrameLayout.setOnTouchListener OnTouchListener@{ _, event ->
+            Log.i("WATCHED", "FrameLayout > X: ${event.x}; Y: ${event.y}; Action: ${MotionEvent.actionToString(event.action)}")
             return@OnTouchListener true
+        }*/
+
+        //Находим точку касания, делаем сдвиг View с поправкой на точку касания
+        myTextView2.setOnTouchListener onTouchListener@{ _, event ->
+            when(event.action){
+                MotionEvent.ACTION_DOWN -> {
+                    x0 = event.x
+                    y0 = event.y
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    myTextView2.translationX += event.x - x0
+                    myTextView2.translationY += event.y - y0
+                }
+            }
+            return@onTouchListener true
         }
     }
 }
